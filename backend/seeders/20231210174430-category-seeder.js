595d88f6b0355c63fa86ef9d6e14b36dc899b744
EdgarEldy <ed.eldy21@live.com>
2023-12-10 20:07:58 +0200

    Add categories data for testing

--- backend/seeders/20231210174430-category-seeder.js
+++ backend/seeders/20231210174430-category-seeder.js
@@ -2,24 +2,39 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
-  },
+    async up(queryInterface, Sequelize) {
+        /**
+         * Add seed commands here.
+         *
+         * Example:
+         * await queryInterface.bulkInsert('People', [{
+         *   name: 'John Doe',
+         *   isBetaMember: false
+         * }], {});
+         */
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+        // Add categories data for testing
+        await queryInterface.bulkInsert('categories', [
+            {
+                category_name: "Lemonades",
+                createdAt: new Date(),
+                updatedAt: new Date()
+            },
+            {
+                category_name: "Alcohols",
+                createdAt: new Date(),
+                updatedAt: new Date()
+            },
+        ], {})
+    },
+
+    async down(queryInterface, Sequelize) {
+        /**
+         * Add commands to revert seed here.
+         *
+         * Example:
+         * await queryInterface.bulkDelete('People', null, {});
+         */
+        await queryInterface.bulkDelete('categories', {})
+    }
 };
