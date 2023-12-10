12a2400aad730cac8aaaa66a43c70c29b21efa4e
EdgarEldy <ed.eldy21@live.com>
2023-12-10 22:53:46 +0200

    Add sidebar shared component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { NavComponent } from './shared/nav/nav.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    NavComponent
+    NavComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
