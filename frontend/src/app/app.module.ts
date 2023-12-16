2848db5031cf1e5c374f31c8ddf43b6044d51304
EdgarEldy <ed.eldy21@live.com>
2023-12-16 11:55:24 +0200

    Add footer component files

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { TopbarComponent } from './shared/topbar/topbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
     SidebarComponent,
-    TopbarComponent
+    TopbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
