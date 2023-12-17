5edb661f4e349d4bf104e0fbfef9fc08d612b08e
EdgarEldy <ed.eldy21@live.com>
2023-12-17 20:57:13 +0200

    Save or update a category

--- frontend/src/app/pages/categories/category-form/category-form.component.ts
+++ frontend/src/app/pages/categories/category-form/category-form.component.ts
@@ -17,4 +17,35 @@ export class CategoryFormComponent {
 
   constructor(private categoryService: CategoryService, public activeModal: NgbActiveModal) {
   }
+
+  // Save or update a category
+  async submit() {
+    if (!this.category.id) {
+      // Create new category
+      const data = {
+        category_name: this.category.category_name
+      };
+
+      await this.categoryService.create(data).subscribe({
+        next: (res) => {
+          console.log(res);
+          this.activeModal.close('Category has updated successfully');
+        },
+        error: (err) => {
+          console.log(err);
+        }
+      });
+    } else {
+      // Update existing category
+      await this.categoryService.update(this.category.id, this.category).subscribe({
+        next: (res) => {
+          console.log(res);
+          this.activeModal.close('Category has saved successfully');
+        },
+        error: (err) => {
+          console.log(err);
+        }
+      });
+    }
+  }
 }
