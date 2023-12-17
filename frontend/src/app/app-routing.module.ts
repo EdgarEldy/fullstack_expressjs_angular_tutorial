f3d2a70894aeecbc645a6f6ec41ba8a1bace4190
EdgarEldy <ed.eldy21@live.com>
2023-12-17 11:26:48 +0200

    Add categories route

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,12 +1,11 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {DashboardComponent} from "./pages/dashboard/dashboard.component";
+import { CategoryListComponent } from './pages/categories/category-list/category-list.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: DashboardComponent
-  }
+  { path: '', component: DashboardComponent },
+  { path: 'categories', component: CategoryListComponent },
 ];
 
 @NgModule({
