f4601b0c9bb9c08ffcae5f34443543130b8c2b62
EdgarEldy <ed.eldy21@live.com>
2023-12-17 20:33:17 +0200

    Add ngbModalOptions: Stop closing the modal by clicking backdrop or outside the modal

--- frontend/src/app/pages/categories/category-list/category-list.component.ts
+++ frontend/src/app/pages/categories/category-list/category-list.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { Category } from '../../../models/category';
 import { CategoryService } from '../../../services/category.service';
 import { CategoryFormComponent } from '../category-form/category-form.component';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal, NgbModalOptions } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-category-list',
@@ -46,8 +46,13 @@ export class CategoryListComponent {
   * We can create or update a category with the same modal form
    */
   openCategoryModal(category?: Category) {
+    let ngbModalOptions: NgbModalOptions = {
+      backdrop: 'static',
+      keyboard: false
+    };
+
     // Open your Bootstrap modal and pass the category to it
-    const modalRef = this.modalService.open(CategoryFormComponent);
+    const modalRef = this.modalService.open(CategoryFormComponent, ngbModalOptions);
     modalRef.componentInstance.category = category;
 
     // Subscribe to the modal's result
