fc7d048b19d3638dfe3662239fd3a84ae1c0bf3e
EdgarEldy <ed.eldy21@live.com>
2023-12-18 15:40:28 +0200

    Initialize Category and Product models

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -4,5 +4,9 @@ const router = express.Router();
 // Initialize database
 const db = require('../models');
 
+// Initialize Category and Product models
+const Category = db.Category;
+const Product = db.Product;
+
 //Exports categories routes
 module.exports = router;
