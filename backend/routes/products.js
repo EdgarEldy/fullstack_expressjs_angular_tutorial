7fef8b5cbf267b60fb254ce7c327d2a5bfb525fa
EdgarEldy <ed.eldy21@live.com>
2023-12-18 16:00:54 +0200

    Get products with their categories api

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -8,5 +8,17 @@ const db = require('../models');
 const Category = db.Category;
 const Product = db.Product;
 
+// Get products with their categories api
+router.get('/products', async (req, res, next) => {
+    const products = await Product.findAll({
+        include: [{
+            model: Category,
+            required: true
+        }]
+    });
+
+    return res.json(products);
+});
+
 //Exports categories routes
 module.exports = router;
