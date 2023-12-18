6853eee51fc2e7557845ad293d388dbd17e56f80
EdgarEldy <ed.eldy21@live.com>
2023-12-18 15:31:19 +0200

    Initialize database

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-
+// Initialize database
+const db = require('../models');
 
 //Exports categories routes
 module.exports = router;
