c1e9572a6377916f2f43f9bf68ef88f6b68390e2
EdgarEldy <ed.eldy21@live.com>
2023-12-19 12:05:18 +0200

    Initialize database connection and Customer model

--- backend/routes/customers.js
+++ backend/routes/customers.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const db = require('../models');
+const Customer = db.Customer;
 
 // Exports customers routes
 module.exports = router;
