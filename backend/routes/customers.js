52bab79c661bc29d4003a7f7fb42ca2214445660
EdgarEldy <ed.eldy21@live.com>
2023-12-19 12:29:54 +0200

    Get all customers api

--- backend/routes/customers.js
+++ backend/routes/customers.js
@@ -4,5 +4,11 @@ const router = express.Router();
 const db = require('../models');
 const Customer = db.Customer;
 
+// Get all customers api
+router.get('/customers', async (req, res, next) => {
+    const customers = await Customer.findAll();
+    return res.json(customers);
+});
+
 // Exports customers routes
 module.exports = router;
