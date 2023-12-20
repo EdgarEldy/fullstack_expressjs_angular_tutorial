47c739fcf2fc51775a62ccd24bc0638ea56d0041
EdgarEldy <ed.eldy21@live.com>
2023-12-20 16:43:13 +0200

    Initialize database and required models

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -1,5 +1,11 @@
 const express = requrire('express');
 const router = express.Router();
 
+// Initialize database and required models
+const db = require('../models');
+
+const customer = db.Customer;
+const Product = db.Product;
+const Order = db.Order;
 
 module.exports = router;
