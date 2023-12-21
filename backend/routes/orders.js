38d8e8bae936a350ed2485893c812a9740562486
EdgarEldy <ed.eldy21@live.com>
2023-12-21 12:13:13 +0200

    Get all orders with customers and products api route

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -8,4 +8,22 @@ const Customer = db.Customer;
 const Product = db.Product;
 const Order = db.Order;
 
+// Get all orders with customers and products api route
+router.get('/orders', async (req, res, next) => {
+    const orders = await Order.findAll({
+        include: [
+            {
+                model: Customer,
+                required: true
+            },
+            {
+                model: Product,
+                required: true
+            }
+        ]
+    });
+
+    return res.json(orders);
+});
+
 module.exports = router;
