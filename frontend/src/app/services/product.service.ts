98484c7fb6f52d31f897300530d068ff917301dd
EdgarEldy <ed.eldy21@live.com>
2024-04-23 11:20:49 +0200

    Inject http client

--- frontend/src/app/services/product.service.ts
+++ frontend/src/app/services/product.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -7,6 +8,6 @@ export class ProductService {
   // Initialize products api
   private readonly baseUrl: string = 'http://localhost:3000/api/products';
 
-  constructor() {
+  constructor(private readonly http: HttpClient) {
   }
 }
