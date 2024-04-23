72aeeb832fdf5f4efb8d589cf57892767431d11b
EdgarEldy <ed.eldy21@live.com>
2024-04-23 14:52:12 +0200

    Inject productService, modalService and toastr

--- frontend/src/app/pages/products/product-list/product-list.component.ts
+++ frontend/src/app/pages/products/product-list/product-list.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ProductService } from '../../../services/product.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-product-list',
@@ -6,5 +9,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
-
+  // Constructor
+  constructor(
+    productService: ProductService,
+    modalService: NgbModal,
+    toastr: ToastrService
+  ) {
+  }
 }
