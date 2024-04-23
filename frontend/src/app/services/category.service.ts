7f4251450ae142e6d29327c060a3b098fcdcdfa1
EdgarEldy <ed.eldy21@live.com>
2024-04-23 11:04:16 +0200

    Update baseUrl api

--- frontend/src/app/services/category.service.ts
+++ frontend/src/app/services/category.service.ts
@@ -8,33 +8,33 @@ import { Category } from '../models/category';
 })
 export class CategoryService {
   // Initialize categories api
-  private apiUrl = 'http://localhost:3000/categories';
+  private baseUrl: string = 'http://localhost:3000/api/categories';
 
   constructor(private http: HttpClient) {
   }
 
   // Get all categories
   getAll(): Observable<Category[]> {
-    return this.http.get<Category[]>(this.apiUrl);
+    return this.http.get<Category[]>(this.baseUrl);
   }
 
   // Get category details by id
   get(id: any): Observable<Category> {
-    return this.http.get<Category>(`${this.apiUrl}/${id}`);
+    return this.http.get<Category>(`${this.baseUrl}/${id}`);
   }
 
   // Create a new category
   create(data: any): Observable<any> {
-    return this.http.post(this.apiUrl, data);
+    return this.http.post(this.baseUrl, data);
   }
 
   // Update an existing category
   update(id: any, data: any): Observable<any> {
-    return this.http.put(`${this.apiUrl}/${id}`, data);
+    return this.http.put(`${this.baseUrl}/${id}`, data);
   }
 
   // Remove a category
   delete(id: any): Observable<Category> {
-    return this.http.delete<Category>(`${this.apiUrl}/${id}`);
+    return this.http.delete<Category>(`${this.baseUrl}/${id}`);
   }
 }
