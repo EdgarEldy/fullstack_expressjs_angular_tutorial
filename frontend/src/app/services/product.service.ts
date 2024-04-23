dc9ba4729a907daef07e46d7e0b548ffa59dcf03
EdgarEldy <ed.eldy21@live.com>
2024-04-23 15:23:35 +0200

    Get all products

--- frontend/src/app/services/product.service.ts
+++ frontend/src/app/services/product.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Product } from '../models/product';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,5 +11,10 @@ export class ProductService {
   private readonly baseUrl: string = 'http://localhost:3000/api/products';
 
   constructor(private readonly http: HttpClient) {
+  }
+
+  // Get all products
+  getAll(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.baseUrl);
   }
 }
